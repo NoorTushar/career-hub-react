39a530066ed8ca57b6ffe89a7907b427e0d72998
NoorTushar <tushar.noor.khan@gmail.com>
2024-03-22 23:59:42 +0600

    featured job card more design done + show all button working

--- src/components/FeaturedJob/FeaturedJob.jsx
+++ src/components/FeaturedJob/FeaturedJob.jsx
@@ -1,4 +1,6 @@
 import PropTypes from "prop-types";
+import { SlLocationPin } from "react-icons/sl";
+import { FaDollarSign } from "react-icons/fa";
 
 const FeaturedJob = ({ featuredJob }) => {
    console.log(featuredJob);
@@ -26,8 +28,8 @@ const FeaturedJob = ({ featuredJob }) => {
          </figure>
          <div className="card-body">
             <h2 className="card-title">{job_title}</h2>
-            <p>{company_name}</p>
-            <div className="flex gap-4 text-primary">
+            <p className="mt-2">{company_name}</p>
+            <div className="flex gap-4 text-primary mt-4">
                <button className="border border-primary px-5 py-2 font-bold rounded-lg">
                   {remote_or_onsite}
                </button>
@@ -35,8 +37,22 @@ const FeaturedJob = ({ featuredJob }) => {
                   {job_type}
                </button>
             </div>
-            <div className="card-actions">
-               <button className="btn btn-primary">View Details</button>
+
+            <div className="flex items-center gap-2 flex-wrap mt-4">
+               <div className="flex gap-1 items-center">
+                  <SlLocationPin className="text-primary text-xl font-semibold" />
+                  <p>{location}</p>
+               </div>
+               <div className="flex gap-1 items-center">
+                  <FaDollarSign className="text-primary text-xl font-semibold" />
+                  <p>Salary: {salary}</p>
+               </div>
+            </div>
+
+            <div className="card-actions mt-4">
+               <button className="btn text-white bg-gradient-to-r from-[#7E90FE] to-[#9873FF]">
+                  View Details
+               </button>
             </div>
          </div>
       </div>
--- src/components/FeaturedJobs/FeaturedJobs.jsx
+++ src/components/FeaturedJobs/FeaturedJobs.jsx
@@ -11,7 +11,13 @@ const FeaturedJobs = () => {
          .then((data) => setJobs(data));
    }, []);
 
-   const jobContainer = jobs.map((eachJob) => {
+   const [dataLength, setDataLength] = useState(4);
+
+   const handleShowAll = () => {
+      setDataLength(jobs.length);
+   };
+
+   const jobContainer = jobs.slice(0, dataLength).map((eachJob) => {
       return <FeaturedJob featuredJob={eachJob} key={eachJob.id}></FeaturedJob>;
    });
 
@@ -23,6 +29,19 @@ const FeaturedJobs = () => {
          ></CommonSectionHeading>
 
          <div className="grid md:grid-cols-2 gap-6">{jobContainer}</div>
+
+         <div
+            className={`text-center mt-8 ${
+               dataLength === jobs.length && "hidden"
+            }`}
+         >
+            <button
+               onClick={handleShowAll}
+               className="btn text-white bg-gradient-to-r from-[#7E90FE] to-[#9873FF]"
+            >
+               See All Jobs
+            </button>
+         </div>
       </>
    );
 };
